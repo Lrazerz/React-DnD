50e8573cb2a8657f7c67f4aa946be3a27cb65077
lrazerz <lrazerz9ine@gmail.com>
2020-09-02 05:33:27 +0300

    added build folder to run from index.html file

---
+++ build/precache-manifest.e37d09cd6aec0f43f72b9ebf26dcac95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1606b28d6b59c58b65f2805045f7834",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb2b4b4b5b51bd5b53ac",
+    "url": "/static/css/main.b8852783.chunk.css"
+  },
+  {
+    "revision": "07b4fdce3a11f1fece42",
+    "url": "/static/js/2.cbe7193a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cbe7193a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb2b4b4b5b51bd5b53ac",
+    "url": "/static/js/main.0f9fd27a.chunk.js"
+  },
+  {
+    "revision": "e24212937c961493d309",
+    "url": "/static/js/runtime-main.3cdf2ca3.js"
+  }
+]);
